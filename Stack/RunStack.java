e7f22c17124db0713dcda14f2406b906b135cd18
Andy <48250847+andybeardness@users.noreply.github.com>
2021-02-17 16:12:57 +0300

    Stack worked!

---
+++ Stack/RunStack.java
@@ -0,0 +1,126 @@
+public class RunStack {
+    public static void main(String[] args) {
+        runTests();
+    }
+
+    public static void runTests() {
+        defaultStack();
+        tenItemsStack();
+        fullAndEmptyStack();
+    }
+
+    public static void show(String str) {
+        System.out.println(str);
+    }
+
+    public static void showEnd() {
+        System.out.println();
+        System.out.println("= = = = = = = = = = = =");
+        System.out.println("=      END  TEST      =");
+        System.out.println("= = = = = = = = = = = =");
+        System.out.println();
+    }
+
+    public static void defaultStack() {
+        SelfStack<String> strStack = new SelfStack<>();
+
+        strStack.push("A");
+
+        show(strStack.toString());
+
+        show(strStack.pop());
+
+        show(strStack.toString());
+
+        strStack.push("A");
+
+        show(strStack.toString());
+
+        show(strStack.pop());
+
+        show(strStack.toString());
+
+        showEnd();
+    }
+
+    public static void tenItemsStack() {
+        int size = 10;
+        SelfStack<String> strStack = new SelfStack<>(size);
+
+        show(strStack.toString());
+
+        strStack.push("A1");
+        strStack.push("A2");
+        strStack.push("A3");
+
+        show(strStack.toString());
+
+        strStack.push("A4");
+        strStack.push("A5");
+        strStack.push("A6");
+        strStack.push("A7");
+
+        show(strStack.toString());
+
+        strStack.push("A8");
+        strStack.push("A9");
+        strStack.push("A10");
+
+        show(strStack.toString());
+
+        show(strStack.pop());
+        show(strStack.pop());
+        show(strStack.pop());
+
+        show(strStack.toString());
+
+        strStack.push("AN1");
+        strStack.push("AN2");
+        strStack.push("AN3");
+
+        show(strStack.toString());
+
+        show(strStack.pop());
+
+        show(strStack.toString());
+
+        showEnd();
+    }
+
+    public static void fullAndEmptyStack() {
+        int size = 5;
+        SelfStack<String> strStack = new SelfStack<>(size);
+
+        show(strStack.toString());
+
+        strStack.push("A1");
+        strStack.push("A2");
+        strStack.push("A3");
+        strStack.push("A4");
+        strStack.push("A5");
+
+        show(strStack.toString());
+
+        show(strStack.pop());
+        show(strStack.pop());
+        show(strStack.pop());
+        show(strStack.pop());
+        show(strStack.pop());
+
+        show(strStack.toString());
+
+        strStack.push("AN1");
+        strStack.push("AN2");
+        strStack.push("AN3");
+        strStack.push("AN4");
+        strStack.push("AN5");
+
+        show(strStack.toString());
+
+        showEnd();
+    }
+
+    public static void tryOutOfBounds() {
+        
+    }
+}
--- Stack/SelfStack.java
+++ Stack/SelfStack.java
@@ -3,18 +3,16 @@ import java.util.Arrays;
 class SelfStack<T> implements SelfStackInterface<T> {
     private int size;
     private T[] stack;
-    private int current;
+    private int current = 0;
 
     public SelfStack() {
         this.size = 1;
         this.stack = initStack(this.size);
-        this.current = 0;
     }
 
     public SelfStack(int size) {
         this.size = size;
         this.stack = initStack(size);
-        this.current = 0;
     }
 
     @Override
@@ -30,32 +28,16 @@ class SelfStack<T> implements SelfStackInterface<T> {
         return (T[]) new Object[this.size];
     }
 
-    private void resize(int newSize) {
-        T[] oldStack = stack;
-        size = newSize;
-        stack = initStack(size);
-        int counter = 0;
-        for (T t: oldStack) {
-            if (t != null) {
-                stack[counter++] = t;
-            }
-        }
-        current = counter;
-    }
-
     @Override
     public void push(T obj) {
-        if (current == size - 1) {
-            stack[current] = obj;
-        }
-        else {
-            stack[current++] = obj;
-        }
+        stack[current++] = obj;
     }
 
     @Override
     public T pop() {
-        stack[current]
-        return stack[current--];
+        if (current == 0) return null;
+        T result = stack[--current];
+        stack[current] = null;
+        return result;
     }
 }
